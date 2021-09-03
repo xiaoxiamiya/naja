e939922d755b28ec4afdc95b7067c45a98e9282f
高豪亮 <gaohlsilence@163.com>
2021-09-03 18:10:29 +0800

    feat(pynaja):修改mongo连接方式

--- pynaja/common/base.py
+++ pynaja/common/base.py
@@ -1,3 +1,6 @@
+import base64
+import binascii
+import calendar
 import copy
 import functools
 import hashlib
@@ -5,23 +8,35 @@ import math
 import pickle
 import platform
 import random
+import re
+import socket
 import sys
 import textwrap
 import itertools
 import os
 import time
 import traceback
+import unicodedata
 import uuid
+import warnings
 import weakref
 import zlib
-from contextlib import contextmanager
+from contextlib import contextmanager, closing
 from datetime import datetime, timedelta
+from collections import Iterable, OrderedDict
 from contextvars import ContextVar
+import urllib
+from typing import List
+from zipfile import ZipFile, ZIP_DEFLATED
 
+import psutil
+import xmltodict
+import xml.dom.minidom
 import jwt
 import pytz
 import loguru
 import ujson
+from cachetools import TTLCache, cached
 
 from pynaja.common.metaclass import Singleton
 
@@ -33,7 +48,12 @@ class _Utils(Singleton):
 
     """
     _BYTES_TYPES = (bytes, type(None))
+
     _STRING_TYPES = (str, type(None))
+
+    _FALSE_VALUES = {r'null', r'none', r'nil', r'false', r'0', r'', False, 0}
+
+    SAFE_STRING_BASE = r'2346789BCEFGHJKMPQRTVWXY'
 
     math = math
     random = random
@@ -43,6 +63,7 @@ class _Utils(Singleton):
     path = os.path
 
     log = loguru.logger
+    urlparse = urllib.parse
 
     deepcopy = staticmethod(copy.deepcopy)
 
@@ -63,6 +84,24 @@ class _Utils(Singleton):
             r'python': sys.version,
             r'system': [platform.system(), platform.release(), platform.version(), platform.machine()],
         }
+
+    @classmethod
+    def deprecation_warning(cls, val):
+
+        warnings.warn(val, DeprecationWarning)
+
+    @classmethod
+    def utf8(cls, val):
+
+        if isinstance(val, cls._BYTES_TYPES):
+            return val
+
+        if isinstance(val, str):
+            return val.encode(r'utf-8')
+
+        raise TypeError(
+            r'Expected str, bytes, or None; got %r' % type(val)
+        )
 
     @classmethod
     def basestring(cls, val):
@@ -88,24 +127,6 @@ class _Utils(Singleton):
         return ujson.loads(cls.basestring(val), **kwargs)
 
     @classmethod
-    def pickle_dumps(cls, val):
-
-        stream = pickle.dumps(val)
-
-        result = zlib.compress(stream)
-
-        return result
-
-    @classmethod
-    def pickle_loads(cls, val):
-
-        stream = zlib.decompress(val)
-
-        result = pickle.loads(stream)
-
-        return result
-
-    @classmethod
     def today(cls, origin=False):
 
         result = datetime.now()
@@ -120,12 +141,200 @@ class _Utils(Singleton):
         return result.replace(hour=0, minute=0, second=0, microsecond=0) if origin else result
 
     @classmethod
+    def utcnow(cls, origin=False):
+
+        result = datetime.utcnow()
+
+        return result.replace(hour=0, minute=0, second=0, microsecond=0) if origin else result
+
+    @classmethod
     def timestamp(cls, msec=False):
 
         if msec:
             return int(time.time() * 1000)
         else:
             return int(time.time())
+
+    @classmethod
+    def convert_bool(cls, val):
+
+        if isinstance(val, str):
+            val = val.lower()
+
+        return val not in cls._FALSE_VALUES
+
+    @classmethod
+    def convert_int(cls, val, default=0):
+
+        result = default
+
+        try:
+            if not isinstance(val, float):
+                result = int(val)
+        except Exception as _:
+            pass
+
+        return result
+
+    @classmethod
+    def convert_float(cls, val, default=0.0):
+
+        result = default
+
+        try:
+            if not isinstance(val, float):
+                result = float(val)
+        except Exception as _:
+            pass
+
+        return result
+
+    @classmethod
+    def interval_limit(cls, val, min_val, max_val):
+
+        result = val
+
+        if min_val is not None:
+            result = max(result, min_val)
+
+        if max_val is not None:
+            result = min(result, max_val)
+
+        return result
+
+    @classmethod
+    def split_int(cls, val, sep=r',', minsplit=0, maxsplit=-1):
+
+        result = [int(item.strip()) for item in val.split(sep, maxsplit) if item.strip().isdigit()]
+
+        fill = minsplit - len(result)
+
+        if fill > 0:
+            result.extend(0 for _ in range(fill))
+
+        return result
+
+    @classmethod
+    def join_int(cls, iterable, sep=r','):
+
+        result = []
+
+        for item in iterable:
+
+            cls_type = type(item)
+
+            if cls_type is int:
+
+                result.append(str(item))
+
+            elif cls_type is str:
+
+                item = item.strip()
+
+                if item.isdigit():
+                    result.append(item)
+
+        return sep.join(result)
+
+    @classmethod
+    def split_str(cls, val, sep=r'|', minsplit=0, maxsplit=-1):
+
+        if val:
+            result = [item.strip() for item in val.split(sep, maxsplit)]
+        else:
+            result = []
+
+        fill = minsplit - len(result)
+
+        if fill > 0:
+            result.extend(r'' for _ in range(fill))
+
+        return result
+
+    @classmethod
+    def join_str(cls, iterable, sep=r'|'):
+
+        return sep.join(str(val).replace(sep, r'') for val in iterable)
+
+    @classmethod
+    def list_extend(cls, iterable, val):
+
+        if cls.is_iterable(val, True):
+            iterable.extend(val)
+        else:
+            iterable.append(val)
+
+    @classmethod
+    def str_len(cls, str_val):
+
+        result = 0
+
+        for val in str_val:
+
+            if unicodedata.east_asian_width(val) in (r'A', r'F', r'W'):
+                result += 2
+            else:
+                result += 1
+
+        return result
+
+    @classmethod
+    def sub_str(cls, str_val, length, suffix=r'...'):
+
+        result = []
+        strlen = 0
+
+        for val in str_val:
+
+            if unicodedata.east_asian_width(val) in (r'A', r'F', r'W'):
+                strlen += 2
+            else:
+                strlen += 1
+
+            if strlen > length:
+
+                if suffix:
+                    result.append(suffix)
+
+                break
+
+            result.append(val)
+
+        return r''.join(result)
+
+    @classmethod
+    def re_match(cls, pattern, value):
+
+        result = re.match(pattern, value)
+
+        return True if result else False
+
+    @classmethod
+    def get_host_ip(cls):
+
+        result = None
+
+        with closing(socket.socket(socket.AF_INET, socket.SOCK_DGRAM)) as _socket:
+            _socket.connect((r'223.5.5.5', 53))
+            result = _socket.getsockname()[0]
+
+        return result
+
+    @classmethod
+    def ip2int(cls, val):
+
+        try:
+            return int(binascii.hexlify(socket.inet_aton(val)), 16)
+        except socket.error:
+            return int(binascii.hexlify(socket.inet_pton(socket.AF_INET6, val)), 16)
+
+    @classmethod
+    def int2ip(cls, val):
+
+        try:
+            return socket.inet_ntoa(binascii.unhexlify(r'%08x' % val))
+        except socket.error:
+            return socket.inet_ntop(socket.AF_INET6, binascii.unhexlify(r'%032x' % val))
 
     @classmethod
     def time2stamp(cls, time_str, format_type=r'%Y-%m-%d %H:%M:%S', timezone=None):
@@ -147,29 +356,138 @@ class _Utils(Singleton):
             return time.strftime(format_type, datetime.fromtimestamp(time_int, pytz.timezone(timezone)).timetuple())
 
     @classmethod
-    def split_int(cls, val, sep=r',', minsplit=0, maxsplit=-1):
+    def xml_encode(cls, dict_val, root_tag=r'root'):
 
-        result = [int(item.strip()) for item in val.split(sep, maxsplit) if item.strip().isdigit()]
+        xml_doc = xml.dom.minidom.Document()
 
-        fill = minsplit - len(result)
+        root_node = xml_doc.createElement(root_tag)
+        xml_doc.appendChild(root_node)
 
-        if fill > 0:
-            result.extend(0 for _ in range(fill))
+        def _convert(_doc, _node, _dict):
+
+            for key, val in _dict.items():
+
+                temp = _doc.createElement(key)
+
+                if isinstance(val, dict):
+                    _convert(_doc, temp, val)
+                else:
+                    temp.appendChild(_doc.createTextNode(str(val)))
+
+                _node.appendChild(temp)
+
+        _convert(xml_doc, root_node, dict_val)
+
+        return xml_doc
+
+    @classmethod
+    def xml_decode(cls, val):
+
+        return xmltodict.parse(cls.utf8(val))
+
+    @classmethod
+    def b32_encode(cls, val, standard=False):
+
+        val = cls.utf8(val)
+
+        result = base64.b32encode(val)
+
+        if not standard:
+            result = result.rstrip(b'=')
+
+        return cls.basestring(result)
+
+    @classmethod
+    def b32_decode(cls, val, standard=False, for_bytes=False):
+
+        val = cls.utf8(val)
+
+        if not standard:
+
+            num = len(val) % 8
+
+            if num > 0:
+                val = val + b'=' * (8 - num)
+
+        result = base64.b32decode(val)
+
+        if for_bytes:
+            return result
+        else:
+            return cls.basestring(result)
+
+    @classmethod
+    def b64_encode(cls, val, standard=False):
+
+        val = cls.utf8(val)
+
+        if standard:
+
+            result = base64.b64encode(val)
+
+        else:
+
+            result = base64.urlsafe_b64encode(val)
+
+            result = result.rstrip(b'=')
+
+        return cls.basestring(result)
+
+    @classmethod
+    def b64_decode(cls, val, standard=False, for_bytes=False):
+
+        val = cls.utf8(val)
+
+        if standard:
+
+            result = base64.b64decode(val)
+
+        else:
+
+            num = len(val) % 4
+
+            if num > 0:
+                val = val + b'=' * (4 - num)
+
+            result = base64.urlsafe_b64decode(val)
+
+        if for_bytes:
+            return result
+        else:
+            return cls.basestring(result)
+
+    @classmethod
+    def jwt_encode(cls, val, key, algorithms=r'HS256'):
+
+        result = jwt.encode(val, key, algorithms)
+
+        return cls.basestring(result)
+
+    @classmethod
+    def jwt_decode(cls, val, key, algorithms=None):
+
+        if not algorithms:
+            algorithms = [r'HS256']
+
+        val = cls.utf8(val)
+
+        return jwt.decode(val, key, algorithms)
+
+    @classmethod
+    def pickle_dumps(cls, val):
+
+        stream = pickle.dumps(val)
+
+        result = zlib.compress(stream)
 
         return result
 
     @classmethod
-    def split_str(cls, val, sep=r'|', minsplit=0, maxsplit=-1):
+    def pickle_loads(cls, val):
 
-        if val:
-            result = [item.strip() for item in val.split(sep, maxsplit)]
-        else:
-            result = []
+        stream = zlib.decompress(val)
 
-        fill = minsplit - len(result)
-
-        if fill > 0:
-            result.extend(r'' for _ in range(fill))
+        result = pickle.loads(stream)
 
         return result
 
@@ -177,19 +495,6 @@ class _Utils(Singleton):
     def uuid1(cls, node=None, clock_seq=None):
 
         return uuid.uuid1(node, clock_seq).hex
-
-    @classmethod
-    def utf8(cls, val):
-
-        if isinstance(val, cls._BYTES_TYPES):
-            return val
-
-        if isinstance(val, str):
-            return val.encode(r'utf-8')
-
-        raise TypeError(
-            r'Expected str, bytes, or None; got %r' % type(val)
-        )
 
     @classmethod
     def md5(cls, val):
@@ -213,7 +518,68 @@ class _Utils(Singleton):
         return int(hashlib.md5(val).hexdigest(), 16) >> 64
 
     @classmethod
-    def params_join(cls, params, filters=[]):
+    def sha1(cls, val):
+
+        val = cls.utf8(val)
+
+        return hashlib.sha1(val).hexdigest()
+
+    @classmethod
+    def sha256(cls, val):
+
+        val = cls.utf8(val)
+
+        return hashlib.sha256(val).hexdigest()
+
+    @classmethod
+    def sha512(cls, val):
+
+        val = cls.utf8(val)
+
+        return hashlib.sha512(val).hexdigest()
+
+    @classmethod
+    def ordered_dict(cls, val=None):
+
+        if val is None:
+            return OrderedDict()
+        else:
+            return OrderedDict(sorted(val.items(), key=lambda x: x[0]))
+
+    @classmethod
+    def is_iterable(cls, obj, standard=False):
+
+        if standard:
+            result = isinstance(obj, Iterable)
+        else:
+            result = isinstance(obj, (list, tuple,))
+
+        return result
+
+    @classmethod
+    def identity_card(cls, val):
+
+        result = False
+
+        val = val.strip().upper()
+
+        if len(val) == 18:
+
+            weight_factor = (7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2, 1,)
+
+            verify_list = r'10X98765432'
+
+            check_sum = 0
+
+            for index in range(17):
+                check_sum += int(val[index]) * weight_factor[index]
+
+            result = (verify_list[check_sum % 11] == val[17])
+
+        return result
+
+    @classmethod
+    def params_join(cls, params, filters: List = None):
 
         if filters:
             params = {key: val for key,
@@ -233,6 +599,109 @@ class _Utils(Singleton):
             result.append(cls.params_join(kwargs))
 
         return cls.md5(r'&'.join(result))
+
+    @classmethod
+    def get_today_region(cls, today=None):
+
+        if today is None:
+            today = cls.today(True)
+
+        start_date = today
+        end_date = today + timedelta(days=1)
+
+        start_time = int(time.mktime(start_date.timetuple()))
+        end_time = int(time.mktime(end_date.timetuple())) - 1
+
+        return start_time, end_time
+
+    @classmethod
+    def get_month_region(cls, today=None):
+
+        if today is None:
+            today = cls.today(True)
+
+        start_date = today.replace(day=1)
+
+        _, days_in_month = calendar.monthrange(
+            start_date.year, start_date.month)
+
+        end_date = start_date + timedelta(days=days_in_month)
+
+        start_time = int(time.mktime(start_date.timetuple()))
+        end_time = int(time.mktime(end_date.timetuple())) - 1
+
+        return start_time, end_time
+
+    @classmethod
+    def get_week_region(cls, today=None):
+
+        if today is None:
+            today = cls.today(True)
+
+        week_pos = today.weekday()
+
+        start_date = today - timedelta(days=week_pos)
+        end_date = today + timedelta(days=(7 - week_pos))
+
+        start_time = int(time.mktime(start_date.timetuple()))
+        end_time = int(time.mktime(end_date.timetuple())) - 1
+
+        return start_time, end_time
+
+    @classmethod
+    def zip_file(cls, zip_file, *file_paths):
+
+        def _add_to_zip(zf, path, zippath):
+
+            if os.path.isfile(path):
+
+                zf.write(path, zippath, ZIP_DEFLATED)
+
+            elif os.path.isdir(path):
+
+                if zippath:
+                    zf.write(path, zippath)
+
+                for nm in os.listdir(path):
+                    _add_to_zip(zf, os.path.join(path, nm),
+                                os.path.join(zippath, nm))
+
+        with ZipFile(zip_file, r'w') as _zf:
+
+            for path in file_paths:
+
+                zippath = os.path.basename(path)
+
+                if not zippath:
+                    zippath = os.path.basename(os.path.dirname(path))
+                if zippath in (r'', os.curdir, os.pardir):
+                    zippath = r''
+
+                _add_to_zip(_zf, path, zippath)
+
+    @classmethod
+    def unzip_file(cls, zip_file, file_paths):
+
+        with ZipFile(zip_file, r'r') as _zf:
+            _zf.extractall(file_paths)
+
+    @classmethod
+    @cached(cache=TTLCache(maxsize=0xf, ttl=1))
+    def get_cpu_percent(cls):
+
+        return psutil.cpu_percent()
+
+    @classmethod
+    def kill_process(cls, *args):
+
+        process_names = [val.lower() for val in args]
+
+        for pid in psutil.pids():
+
+            process = psutil.Process(pid)
+
+            if process.name().lower() in process_names:
+                process.kill()
 
     @staticmethod
     def chunks_content_n(arr, n):
@@ -289,22 +758,6 @@ class _Utils(Singleton):
             data_list.append(data)
 
         return new_data
-
-    @classmethod
-    def jwt_encode(cls, val, key, algorithms=r'HS256'):
-
-        result = jwt.encode(val, key, algorithms)
-
-        return cls.basestring(result)
-
-    @classmethod
-    def jwt_decode(cls, val, key, algorithms=None):
-        if not algorithms:
-            algorithms = [r'HS256']
-
-        val = cls.utf8(val)
-
-        return jwt.decode(val, key, algorithms)
 
 
 @contextmanager
